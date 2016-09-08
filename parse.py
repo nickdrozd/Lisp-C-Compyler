08cd5232fa9d29d50f780c12ab7cde41dabbed6b
Nick Drozd <nicholasdrozd@gmail.com>
2016-09-07 20:40:02 -0500

    added schemify
    aka Norvig's "schemestr"

--- parse.py
+++ parse.py
@@ -8,9 +8,20 @@ in the ass and this one is at least as good
 as any of mine.)
 '''
 
+# primary functions
+
 def parse(program):
     "Read a Scheme expression from a string."
     return read_from_tokens(tokenize(program))
+
+def schemify(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemify, exp)) + ')' 
+    else:
+        return str(exp)
+
+# helpers
 
 def tokenize(chars):
     "Convert a string of characters into a list of tokens."
@@ -43,3 +54,11 @@ def atom(token):
 Symbol = str          # A Scheme Symbol is implemented as a Python str
 List   = list         # A Scheme List is implemented as a Python list
 Number = (int, float) # A Scheme Number is implemented as a Python int or float
+
+
+def schemestr(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
