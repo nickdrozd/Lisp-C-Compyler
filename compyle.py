ebabee134319959ab07e4d3ecbb637564c5ce804
Nick Drozd <nicholasdrozd@gmail.com>
2016-09-10 17:37:31 -0500

    setting up final printing
    TODO: figure out how to generate the whole required C file in one go

--- compyle.py
+++ compyle.py
@@ -2,6 +2,7 @@ from parse import parse
 from compileDisp import compileDisp
 from instructions import statements
 from keywords import *
+from labels import labels
 
 
 def compyle(exprSeq):
@@ -14,7 +15,27 @@ def compyle(exprSeq):
 
 		for line in code:
 			print(line)
-			print('\n')
+
+	print('\n\ntypedef enum {')
+	for label in labels:
+		print('\t_' + label + ',')
+	print('}')
+
+	print("\n\nCOMP_LABEL:\n")
+	for label in labels:
+		print('\tif (GETLABEL(val) == _' + label + ')')
+		print('\t\tgoto ' + label + ';')
+
+	print("\n\nCOMP_LABEL:\n")
+	for label in labels:
+		print('\tif (GETLABEL(cont) == _' + label + ')')
+		print('\t\tgoto ' + label + ';')
+
+	print('\n\nprint_label\n')
+	for label in labels:
+		print('case _' + label + ':')
+		print('\tprintf(\"' + label + ' \");')
+		print('\tbreak;')
 
 	print('\n')
 
@@ -26,9 +47,13 @@ def compyle(exprSeq):
 # 	'(define b 0)',
 # 	'(define c (if (if p q r) a b))',
 # 	'(define f (lambda (x) 5))'
+#	'(define factorial (lambda (n) (if (= n 1) 1 (* (factorial (- n 1) n)))))'
+#	'(define fib (lambda (n) (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2))))))'
 # ]
 
-
-exprSeq = ['(f 5 6)']
+exprSeq = [
+'(define add1 (lambda (n) (+ n 1)))',
+'(add1 5)'
+]
 
 compyle(exprSeq)
