adaff98893e53f04f8fc07216430c0cd73eb5196
Nick Drozd <nicholasdrozd@gmail.com>
2016-09-15 12:41:29 -0500

    added compylePrint for quick testing

--- compyle.py
+++ compyle.py
@@ -7,8 +7,6 @@ FILE WILL BE OVERWRITTEN IF IT ALREADY EXISTS.
 The file is set to print the strings in library.py, 
 but simpler sequences can be used. For instance, try
 compyle(['(define f (lambda () 5))', '(define x (f))']).
-
-TODO: figure out how to get the output into a file!
 '''
 
 from parse import parse
@@ -78,63 +76,32 @@ def compyle(exprSeq):
 
 	comp_code.write('\n\n' + '#endif' + '\n')
 
-
-
 compyle(library)
-# compyle(['((addn 4) 5)'])
-# compyle(['(f 1 2 3 4 5 6 7 8)'])
+
+''''''
+
+# for testing
+def compylePrint(exprSeq):
+
+	for expr in exprSeq:
+		parsed = parse(expr)
+		compiled = compileDisp(parsed)
+		code = statements(compiled)
+
+		for line in code:
+			print(line)
+
+exprSeq = [
+	# '((addn 4) 5)',
+	# '(f 1 2 3 4 5 6 7 8)',
+	# '(lambda (s) s)'
+	'(lambda s s)'
+]
+
+# compylePrint(exprSeq)
 
 
 
-""" SAVE THIS """
-
-# def compyle(exprSeq):
-# 	print('\n')
-
-# 	# print('#define COMPILED_CODE_BODY \\')
-# 	for expr in exprSeq:
-# 		parsed = parse(expr)
-# 		compiled = compileDisp(parsed)
-# 		code = statements(compiled)
-
-# 		for line in code:
-# 			print(line)
-
-	# print('goto DONE;')
-
-	# labelsLen = len(labels)
-	# print('#define ALL_COMPILED_LABELS \\')
-	# for label in labels:
-	# 	if labels.index(label) == labelsLen - 1:
-	# 		print('_' + label)
-	# 	else:
-	# 		print('_' + label + ',' + ' \\')
-
-	# for label in labels:
-	# 	print('\t_' + label + ', ')
-
-	# for label in labels:
-	# 	print('if (GETLABEL(REG) == _' + label + ')' + ' \\')
-	# 	print('goto ' + label + ';' + ' \\')
-
-	# print("\n\nCOMP_LABEL:\n")
-	# for label in labels:
-	# 	print('\t\tif (GETLABEL(val) == _' + label + ')')
-	# 	print('\t\t\tgoto ' + label + ';')
-
-	# print("\n\nCONTINUE:\n")
-	# print('// compiler labels')
-	# for label in labels:
-	# 	print('\t\tif (GETLABEL(cont) == _' + label + ')')
-	# 	print('\t\t\tgoto ' + label + ';')
-
-	# print('\n\nprint_label\n')
-	# for label in labels:
-	# 	print('case _' + label + ':')
-	# 	print('\tprintf(\"' + label + ' \");')
-	# 	print('\tbreak;')
-
-	# print('\n')
 
 
 
