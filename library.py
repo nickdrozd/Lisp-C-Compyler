5fdad40543cf6beda605fc29a144b03641187821
Nick Drozd <nicholasdrozd@gmail.com>
2016-09-17 17:20:14 -0500

    changed library formatting and added new functions
    also deleted church encodings. maybe include those as part of the interpreted library?

--- library.py
+++ library.py
@@ -1,145 +1,164 @@
-# arithmetic operations
-
-zero_ = (
-	'(define zero? '
-		'(lambda (n) '
-			'(= n 0)))'
-)
-
-add1 = (
-	'(define add1 '
-		'(lambda (n) '
-			'(+ n 1)))'
-)
-
-sub1 = (
-	'(define sub1 '
-		'(lambda (n) '
-			'(- n 1)))'
-)
-
-triangular = (
-	'(define recursive_triangular_compiled '
-		'(lambda (n) '
-			'(if (zero? n) '
-				'0 '
-				'(+ n (recursive_triangular_compiled (sub1 n))))))'
-)
-
-tetrahedral = (
-	'(define recursive_tetrahedral_compiled '
-		'(lambda (n) '
-			'(if (zero? n) '
-				'0 '
-				'(+ (recursive_triangular_compiled n) '
-					'(recursive_tetrahedral_compiled (sub1 n))))))'
-)
-
-supertetrahedral = (
-	'(define recursive_supertetrahedral_compiled '
-		'(lambda (n) '
-			'(if (zero? n) '
-				'0 '
-				'(+ (recursive_tetrahedral_compiled n) '
-					'(recursive_supertetrahedral_compiled (sub1 n))))))'
-)
-
-recursive_factorial = (
-	'(define recursive_factorial_compiled '
-		'(lambda (n) '
-			'(if (zero? n) '
-				'1 '
-				'(* n (recursive_factorial_compiled (sub1 n))))))'
-)
-
-iterative_factorial = (
-	'(define iterative_factorial_compiled '
-		'(lambda (n) '
-			'(define loop '
-				'(lambda (count total) '
-					'(if (zero? count) '
-						'total '
-						'(loop (sub1 count) '
-							'(* total count)))))' 
-			'(loop n 1)))'
-)
-
-recursive_fibonacci = (
-	'(define recursive_fibonacci_compiled '
-		'(lambda (n) '
-			'(if (or (= n 0) '
-					'(= n 1)) '
-				'n '
-				'(+ (recursive_fibonacci_compiled (- n 1)) '
-					'(recursive_fibonacci_compiled (- n 2))))))'
-)
-
-iterative_fibonacci = (
-	'(define iterative_fibonacci_compiled '
-		'(lambda (n) '
-			'(define loop '
-				'(lambda (count a b) '
-					'(if (zero? count) '
-						'a '
-						'(loop (sub1 count) '
-							'b '
-							'(+ a b))))) '
-			'(loop n 0 1)))'
-)
-
-factorial = (
-	'(define factorial iterative_factorial_compiled)'
-)
-
-fibonacci = (
-	'(define fibonacci iterative_fibonacci_compiled)'
-)
-
 # list operations
 
-cons = (
-	'(define cons '
-		'(lambda (x y)'
-			'(lambda (s)'
-				'(s x y))))'
-)
+nil = '''
+(define nil (quote ()))
+'''
 
-car = (
-	'(define car '
-		'(lambda (p) '
-			'(p (lambda (x y) '
-					'x))))'
-)
+list_ = '''
+(define list 
+	(lambda s s))
+'''
 
-cdr = (
-	'(define cdr '
-		'(lambda (p) '
-			'(p (lambda (x y) '
-					'y))))'
-)
+length ='''
+(define length 
+	(lambda (s) 
+		(if (null? s) 
+			0 
+			(add1 (length (cdr s))))))
+'''
 
-nil = (
-	'(define nil (quote ()))'
-)
+listRef = '''
+(define list-ref
+	(lambda (items n)
+		(if (zero? n)
+			(car items)
+			(list-ref (cdr items) (sub1 n)))))
+'''
 
-length = (
-	'(define length '
-		'(lambda (s) '
-			'(if (null? s) '
-				'0 '
-				'(add1 (length (cdr s))))))'
-)
+append = '''
+(define append
+	(lambda (list1 list2)
+		(if (null? list1)
+			list2
+			(cons (car list1)
+				(append (cdr list1) list2)))))
+'''
 
-# libraries
+map_ = '''
+(define map
+	(lambda (func items)
+		(if (null? items)
+			nil
+			(cons (func (car items))
+				(map func (cdr items))))))
+'''
+
+foldRight = '''
+(define fold-right
+	(lambda (comb null seq)
+		(if (null? seq)
+			null
+			(comb (car seq)
+				(fold-right comb null (cdr seq))))))
+'''
+
+
+listLib = [
+nil, list_, length,
+listRef, append, 
+map_, foldRight, 
+]
+
+# arithmetic operations
+
+add = '''
+(define + 
+	(lambda nums
+		(fold-right __+__ 0 nums)))
+'''
+
+mul = '''
+(define * 
+	(lambda nums
+		(fold-right __*__ 1 nums)))
+'''
+
+triangular = '''
+(define recursive_triangular_compiled 
+	(lambda (n) 
+		(if (zero? n) 
+			0 
+			(__+__ n (recursive_triangular_compiled (sub1 n))))))
+'''
+
+tetrahedral = '''
+(define recursive_tetrahedral_compiled 
+	(lambda (n) 
+		(if (zero? n) 
+			0 
+			(__+__ (recursive_triangular_compiled n) 
+				(recursive_tetrahedral_compiled (sub1 n))))))
+'''
+
+supertetrahedral = '''
+(define recursive_supertetrahedral_compiled 
+	(lambda (n) 
+		(if (zero? n) 
+			0 
+			(__+__ (recursive_tetrahedral_compiled n) 
+				(recursive_supertetrahedral_compiled (sub1 n))))))
+'''
+
+recursive_factorial = '''
+(define recursive_factorial_compiled 
+	(lambda (n) 
+		(if (zero? n) 
+			1 
+			(* n (recursive_factorial_compiled (sub1 n))))))
+'''
+
+iterative_factorial = '''
+(define iterative_factorial_compiled 
+	(lambda (n) 
+		(define loop 
+			(lambda (count total) 
+				(if (zero? count) 
+					total 
+					(loop (sub1 count) 
+						(* total count))))) 
+		(loop n 1)))
+'''
+
+recursive_fibonacci = '''
+(define recursive_fibonacci_compiled 
+	(lambda (n) 
+		(if (< n 2)
+			n 
+			(__+__ (recursive_fibonacci_compiled (- n 1)) 
+				(recursive_fibonacci_compiled (- n 2))))))
+'''
+
+iterative_fibonacci = '''
+(define iterative_fibonacci_compiled 
+	(lambda (n) 
+		(define loop 
+			(lambda (count a b) 
+				(if (zero? count) 
+					a 
+					(loop (sub1 count) 
+						b 
+						(__+__ a b))))) 
+		(loop n 0 1)))
+'''
+
+factorial = '''
+(define factorial iterative_factorial_compiled)
+'''
+
+fibonacci = '''
+(define fibonacci iterative_fibonacci_compiled)
+'''
+
 
 arithLib = [
-	zero_, add1, sub1,
+	add, mul, 
 	triangular, tetrahedral, supertetrahedral, 
 	recursive_factorial, iterative_factorial, 
 	recursive_fibonacci, iterative_fibonacci,
 	factorial, fibonacci
 ]
 
-listLib = [cons, car, cdr, nil, length]
+
+
 
 library = arithLib + listLib
