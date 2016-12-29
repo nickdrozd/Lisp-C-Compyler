f11f95ff6c748d116df2d75859f6e72e06572037
nickdrozd <nicholasdrozd@gmail.com>
2016-12-29 09:32:33 -0600

    Fix ctext

--- ctext.py
+++ ctext.py
@@ -1,19 +1,23 @@
+# include this?
+from parse import schemify
+
 # make these more general
 
-def CText(instr):
-	return lambda expr, reg: instr.format(expr, reg)
+# or not; it's easy to get these backwards
+
+assText = lambda instr: lambda expr, reg: instr.format(reg, expr)
+
+numText = assText('{} = NUMOBJ({});')
+varText = assText('{} = lookup(NAMEOBJ("{}"), env);')
+quoteText = assText('{} = parse("{}\\n");')
 
 
-numText = CText('{} = NUMOBJ({});')
-
-varText = CText('{} = lookup(NAMEOBJ("{}"), env);')
-
-quoteText = CText('{} = parse("{}\\n");')
-
-
-def svRstText(cmd):
-	instr = '{}({});'
-	return lambda reg: CText(instr, reg)
+svRstText = lambda cmd: lambda reg: '{}({});'.format(cmd, reg) 
 
 saveText = svRstText('save')
 restoreText = svRstText('restore')
+
+
+gotoText = lambda label: 'goto %s;' % label
+
+ifTestText = lambda label: 'if (isTrue(val)) ' + gotoText(label)
--- linkage.py
+++ linkage.py
@@ -1,18 +1,21 @@
 from registers import *
 from instructions import *
+from ctext import *
 
 ret = 'return'
 nex = 'next'
 
 def compileLinkage(linkage):
 	if linkage == ret:
-		return InstrSeq([cont], [], ['goto CONTINUE;'])
+		return InstrSeq([cont], [], [gotoText('CONTINUE')])
 	elif linkage == nex:
 		return InstrSeq([], [], [])
 	else:
-		return InstrSeq([], [], ['goto %(linkage)s;' % locals()])
+		return InstrSeq([], [], [gotoText(linkage)])
 
 
 def endWithLink(linkage, instrSeq):
 	return preserving([cont], instrSeq, 
 						compileLinkage(linkage))
+
+# preserving?
