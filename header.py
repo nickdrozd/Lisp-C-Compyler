795ffd24c14e332a58486e6d242e1eb68c952368
Nick Drozd <nicholasdrozd@gmail.com>
2017-10-01 13:19:51 -0500

    Separate header compilation and file output

--- header.py
+++ header.py
@@ -15,7 +15,7 @@ from library import LIBRARY
 
 
 def make_lispinc_header(expr_seq):
-    comp_code = open('comp_code.h', 'w')
+    comp_code = ''
 
     heading = \
 '''/*
@@ -32,24 +32,24 @@ def make_lispinc_header(expr_seq):
 #define COMPILED_CODE_BODY \\
 '''
 
-    comp_code.write(heading)
+    comp_code += heading
     for expr in expr_seq:
         parsed = parse(expr)
         compiled = comp_exp(parsed)
         code = statements(compiled)
 
         for line in code:
-            comp_code.write(line + ' \\' + '\n')
-    comp_code.write('goto DONE;')
+            comp_code += line + ' \\' + '\n'
+    comp_code += 'goto DONE;'
 
-    comp_code.write('\n\n')
+    comp_code += '\n\n'
 
     def is_last_label(label):
         labels_len = len(LABELS)
         label_index = LABELS.index(label)
         return label_index == labels_len - 1
 
-    comp_code.write('#define COMP_CONT(REG) \\' + '\n')
+    comp_code += '#define COMP_CONT(REG) \\' + '\n'
     for label in LABELS:
         last_label = is_last_label(label)
         label_check = (
@@ -57,11 +57,11 @@ def make_lispinc_header(expr_seq):
             ') ' + 'goto ' + label + ';' +
             ('' if last_label else ' \\' + '\n')
         )
-        comp_code.write(label_check)
+        comp_code += label_check
 
-    comp_code.write('\n\n')
+    comp_code += '\n\n'
 
-    comp_code.write('#define ALL_COMPILED_LABELS \\' + '\n')
+    comp_code += '#define ALL_COMPILED_LABELS \\' + '\n'
     # i = 0
     for label in LABELS:
         undscr_label = '_' + label
@@ -77,9 +77,18 @@ def make_lispinc_header(expr_seq):
             #   listed_label = comma_label + '\\' + '\n'
             #   i = 0
 
-        comp_code.write(listed_label)
+        comp_code += listed_label
 
-    comp_code.write('\n\n' + '#endif' + '\n')
+    comp_code += '\n\n' + '#endif' + '\n'
+
+    return comp_code
 
 
-make_lispinc_header(LIBRARY)
+def write_lispinc_header():
+    with open('comp_code.h', 'w') as comp_code:
+        comp_code.write(
+            make_lispinc_header(LIBRARY))
+
+
+if __name__ == '__main__':
+    write_lispinc_header()
