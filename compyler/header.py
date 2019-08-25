21b0f4e231a8468eb735c894d277285b6a94faa5
Nick Drozd <nicholasdrozd@gmail.com>
2019-08-25 17:32:47 -0500

    Close header fd

--- compyler/header.py
+++ compyler/header.py
@@ -14,9 +14,9 @@ from library import LIBRARY
 
 
 def makeLispincHeader(exprSeq):
-    comp_code = open('comp_code.h', 'w')
+    with open('comp_code.h', 'w') as comp_code:
 
-    heading = '''/*
+        heading = '''/*
     This code is compiler-generated!
     It may be ugly, but it sure is fast!
     Can you figure out how it works?
@@ -30,54 +30,54 @@ def makeLispincHeader(exprSeq):
 #define COMPILED_CODE_BODY \\
 '''
 
-    comp_code.write(heading)
-    for expr in exprSeq:
-        parsed = parse(expr)
-        compiled = compExp(parsed)
-        code = compiled.stmts
+        comp_code.write(heading)
+        for expr in exprSeq:
+            parsed = parse(expr)
+            compiled = compExp(parsed)
+            code = compiled.stmts
 
-        for line in code:
-            comp_code.write(line + ' \\' + '\n')
-    comp_code.write('goto DONE;')
+            for line in code:
+                comp_code.write(line + ' \\' + '\n')
+        comp_code.write('goto DONE;')
 
-    comp_code.write('\n\n')
+        comp_code.write('\n\n')
 
-    def isLastLabel(label):
-        labelsLen = len(LABELS)
-        labelIndex = LABELS.index(label)
-        return labelIndex == labelsLen - 1
+        def isLastLabel(label):
+            labelsLen = len(LABELS)
+            labelIndex = LABELS.index(label)
+            return labelIndex == labelsLen - 1
 
-    comp_code.write('#define COMP_CONT(REG) \\' + '\n')
-    for label in LABELS:
-        lastLabel = isLastLabel(label)
-        labelCheck = (
-            'if (GETLABEL(REG) == _' + label +
-            ') ' + 'goto ' + label + ';' +
-            ('' if lastLabel else ' \\' + '\n')
-        )
-        comp_code.write(labelCheck)
+        comp_code.write('#define COMP_CONT(REG) \\' + '\n')
+        for label in LABELS:
+            lastLabel = isLastLabel(label)
+            labelCheck = (
+                'if (GETLABEL(REG) == _' + label +
+                ') ' + 'goto ' + label + ';' +
+                ('' if lastLabel else ' \\' + '\n')
+            )
+            comp_code.write(labelCheck)
 
-    comp_code.write('\n\n')
+        comp_code.write('\n\n')
 
-    comp_code.write('#define ALL_COMPILED_LABELS \\' + '\n')
-    # i = 0
-    for label in LABELS:
-        undscrLabel = '_' + label
-        if isLastLabel(label):
-            listedLabel = undscrLabel
-        else:
-            listedLabel = undscrLabel + ', \\' + '\n'
-            # commaLabel = undscrLabel + ', '
-            # if i < 2:
-            #   listedLabel = commaLabel
-            #   i += 1
-            # else:
-            #   listedLabel = commaLabel + '\\' + '\n'
-            #   i = 0
+        comp_code.write('#define ALL_COMPILED_LABELS \\' + '\n')
+        # i = 0
+        for label in LABELS:
+            undscrLabel = '_' + label
+            if isLastLabel(label):
+                listedLabel = undscrLabel
+            else:
+                listedLabel = undscrLabel + ', \\' + '\n'
+                # commaLabel = undscrLabel + ', '
+                # if i < 2:
+                #   listedLabel = commaLabel
+                #   i += 1
+                # else:
+                #   listedLabel = commaLabel + '\\' + '\n'
+                #   i = 0
 
-        comp_code.write(listedLabel)
+            comp_code.write(listedLabel)
 
-    comp_code.write('\n\n' + '#endif' + '\n')
+        comp_code.write('\n\n' + '#endif' + '\n')
 
 
 makeLispincHeader(LIBRARY)
